dbe60c29239bb6949068dba4f13bb2162ec3c429
Prashanth Kumar <31966594+PrashanthKumar0@users.noreply.github.com>
2021-09-30 17:05:05 +0530

    Update soundPool.js

--- scripts/soundPool.js
+++ scripts/soundPool.js
@@ -88,14 +88,14 @@ class SoundPool {
             if (this.sounds[i].name == name) {
                 // if (this.sounds[i].aud.paused) {
                 this.sounds[i].aud.volume = volume;
-                this.sounds[i].aud.currentTime = 0;
+                // this.sounds[i].aud.currentTime = 0;
                 if (this.sounds[i].aud.paused) {
                     this.sounds[i].aud.play().then((function () {
                         this.sounds[i].aud._playing = true; // ? what am i doing ?
                         // this.sounds[i].aud.loop = true; // ? what am i doing ?
                     }).bind(this)).catch(function (err) { }); // play main file if all slot is full
 
-                    this.sounds[i].aud.loop = false;
+                    this.sounds[i].aud.loop = true;
                 }
                 // }
             }
